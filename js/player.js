79e812e56f0d45dd9f250055f3c78761d0633045
Antonia Caskey <antonia.caskey@gmail.com>
2016-05-01 14:52:40 +1200

    Created an Array for exhibits to focus on

---
+++ js/player.js
@@ -0,0 +1,40 @@
+
+// List relates the url provided name with the title and soundcloud url
+var audioList = [
+	{
+		id: "shitsfucked",
+		title: "Everybody Knows Shit's Fucked",
+		url: ""
+	},
+	{
+		id: "huria1",
+		title: "The Portrait of Huria Matenga",
+		url: ""
+	},
+	{
+		id: "huria2",
+		title: "The Cloak of Huria Matenga",
+		url: ""
+	},
+	{
+		id: "kupeanchor",
+		title: "Kupe’s Anchor",
+		url: ""
+	},
+	{
+		id: "kuri",
+		title: "The Kuri",
+		url: ""
+	},
+	{
+		id: "bluewhale",
+		title: "The Blue Whale Heart",
+		url: ""
+	},
+	{
+		id: "pharlap",
+		title: "Phar Lap",
+		url: ""
+	}
+
+];
